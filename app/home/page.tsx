f58b79832fa3cf9b3cb2143c8437bb4651cc20b6
tincurar <tincurares13@gmail.com>
2023-10-01 00:53:41 +0300

    added tailwind styling...needs more work

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,6 +4,7 @@ import ComposeTweet from "@/components/client-components/ComposeTweet";
 import Tweet from "@/components/client-components/Tweet";
 import Logout from "../auth/sign-out/Logout";
 import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
@@ -36,25 +37,79 @@ export default async function Home() {
   // CLICK ON REPLY:
   // /REPLYAUTHOR-USERNAME/tweet/[replyId]
 
+  const navigationList = [
+    "Logo",
+    "Home",
+    "Explore",
+    "Notifications",
+    "Messages",
+    "Lists ?",
+    "Communities ?",
+    "Bookmarks (favs)",
+  ];
+
   return (
     <>
-      <div className="flex flex-col items-center mt-16">
-        <h1>Hello World</h1>
-        <h1>-</h1>
-        <h1>-</h1>
-        <Logout />
-        <h1>-</h1>
-        <h1>-</h1>
-        <h2>Create new Tweet</h2>
-        <ComposeTweet user={user} />
-        <h1>-</h1>
-        <h1>-</h1>
-        {tweets?.map((tweet) => (
-          <Tweet key={tweet.id} user={user} tweet={tweet} />
-        ))}
-        <h1>-</h1>
-        <h1>-</h1>
-        <pre>{JSON.stringify(tweets, null, 2)}</pre>
+      <div className="flex">
+        {/* left sidebar */}
+        <div className="flex flex-col max-w-xl flex-grow items-end">
+          <div className="top-0 fixed flex flex-col h-full justify-between px-2">
+            <div className="flex flex-col text-xl">
+              <div className="flex flex-col items-start my-4 mx-4 space-y-4">
+                {navigationList.map((item) => (
+                  <div className="px-4 rounded-full hover:bg-green-500 transition duration-200">
+                    <Link href={`/${item.toLowerCase()}`}>{item}</Link>
+                  </div>
+                ))}
+              </div>
+              <button>Post</button>
+            </div>
+            <div className="self-center mb-2">
+              <Logout />
+            </div>
+          </div>
+        </div>
+
+        {/* infinite tweets feed */}
+        <div className="max-w-[600px] w-full h-full mx-0 border-l border-r">
+          {/* TOP HEADER */}
+          <div className="top-0 sticky flex border-b w-full backdrop-filter backdrop-blur-md bg-opacity-70 bg-slate-950">
+            <div className="flex flex-col w-full">
+              <h1 className="text-lg p-4 font-bold">Home</h1>
+              <div className="flex flex-row">
+                <div className="w-1/2 flex justify-center hover:bg-white/10">
+                  <button className="border-blue-500 border-b-4 py-4">
+                    For You
+                  </button>
+                </div>
+                <div className="w-1/2 flex justify-center hover:bg-white/10">
+                  <button className="hover:border-blue-500 border-transparent border-b-4 py-4">
+                    Following
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* TOP HEADER */}
+          <div className="flex flex-col items-center">
+            <ComposeTweet user={user} />
+            <h1>.</h1>
+            <h1>.</h1>
+            {tweets?.map((tweet) => (
+              <Tweet key={tweet.id} user={user} tweet={tweet} />
+            ))}
+            <h1>-</h1>
+            <h1>-</h1>
+            <pre className="flex flex-wrap w-fit">
+              {JSON.stringify(tweets, null, 2)}
+            </pre>
+          </div>
+        </div>
+
+        {/* right sidebar */}
+        <div>
+          <div className="top-0 fixed">some content</div>
+        </div>
       </div>
     </>
   );
--- components/client-components/ComposeTweet.tsx
+++ components/client-components/ComposeTweet.tsx
@@ -5,8 +5,28 @@ import { useRef } from "react";
 
 const ComposeTweet = ({ user }: any) => {
   const supabase = createClientComponentClient();
-  const tweetTextRef = useRef<HTMLInputElement>(null);
+  const tweetTextRef = useRef<HTMLTextAreaElement>(null);
   const tweetMaxLength = 280;
+  const characterLimit = 350;
+  const maxTextAreaHeight = 300;
+
+  const handleChange = () => {
+    // if (
+    //   tweetTextRef.current &&
+    //   tweetTextRef.current.value.length >= tweetMaxLength
+    // ) {
+    //   tweetTextRef.current.style.color = "red";
+    // } else {
+    //   tweetTextRef.current!.style.color = "black";
+    // }
+    if (tweetTextRef.current) {
+      tweetTextRef.current.style.height = "auto";
+      tweetTextRef.current.style.height = `${Math.min(
+        tweetTextRef.current.scrollHeight,
+        maxTextAreaHeight
+      )}px`;
+    }
+  };
 
   const postTweet = async () => {
     if (
@@ -25,14 +45,35 @@ const ComposeTweet = ({ user }: any) => {
 
   return (
     <>
-      <input
-        ref={tweetTextRef}
-        type="text"
-        style={{ color: "black" }}
-        placeholder="What's happening?"
-      />
+      <div className="flex flex-row p-2 w-full space-x-4 border-b">
+        <div className="rounded-full bg-white w-10 h-10 p-2">img</div>
+        <div className="flex flex-col space-y-4 w-full px-2">
+          <div className="flex flex-col border-b">
+            <textarea
+              ref={tweetTextRef}
+              onChange={handleChange}
+              maxLength={characterLimit}
+              style={{
+                background: "transparent",
+                border: "none",
+                outline: "none",
+                resize: "none",
+                padding: "8px 0 0 0",
+                // add invisible scrollbar
+              }}
+              placeholder="What is happening?!"
+            />
+          </div>
 
-      <button onClick={postTweet}>post</button>
+          <button
+            className="self-end rounded-full bg-blue-500 py-2 px-4"
+            style={{ marginBottom: "8px" }}
+            onClick={postTweet}
+          >
+            Post
+          </button>
+        </div>
+      </div>
     </>
   );
 };
