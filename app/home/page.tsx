9ad3ee0b6ee30a54921c01dcb236e3ee652252fa
tincurar <tincurares13@gmail.com>
2023-10-03 17:15:42 +0300

    need to fix last hover effect and add follow

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,6 +4,8 @@ import { redirect } from "next/navigation";
 import LeftSidebar from "@/components/LeftSidebar";
 import InfiniteFeed from "@/components/InfiniteFeed";
 import { BiSearch } from "react-icons/bi";
+import Image from "next/image";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
@@ -14,6 +16,9 @@ export default async function Home() {
     data: { user },
   } = await supabase.auth.getUser();
 
+  if (!user) {
+    redirect("/");
+  }
   const { data, error } = await supabase
     .from("tweets")
     .select("*, author: profiles(*), likes(*)")
@@ -27,9 +32,9 @@ export default async function Home() {
     likes: tweet.likes.length,
   }));
 
-  if (!user) {
-    redirect("/");
-  }
+  const { data: followProfiles, error: followProfilesError } = await supabase
+    .from("profiles")
+    .select("*");
 
   // IMPORTANT:
   // ROUTING: - dynamic tweet page
@@ -45,9 +50,9 @@ export default async function Home() {
         <InfiniteFeed user={user} tweets={tweets} />
 
         {/* right sidebar */}
-        <div>
-          <div className="top-0 fixed py-2">
-            <div className="group flex bg-[#16181C] rounded-full p-2 mx-4 px-4 items-center space-x-3 border focus-within:bg-slate-950 focus-within:border-blue-500">
+        <div className="flex flex-col max-w-[350px] w-full mx-4">
+          <div className="top-0 fixed py-2 flex flex-col max-w-[350px] w-full">
+            <div className="group flex bg-[#16181C] rounded-full p-2  px-4 items-center space-x-3 border focus-within:bg-slate-950 focus-within:border-blue-500">
               <BiSearch className="group-focus-within:text-blue-500" />
               <input
                 type="text"
@@ -55,14 +60,41 @@ export default async function Home() {
                 className="bg-[#16181C] outline-none focus:bg-slate-950"
               />
             </div>
-            <div className="flex flex-col bg-[#16181C] rounded-xl mx-4 px-4 p-2 mt-5 space-y-4">
-              <h2>Who to Follow</h2>
-              <div>Profile 1</div>
-              <div>Profile 2</div>
-              <div>Profile 3</div>
-              <div>Profile 4</div>
+            <div className="flex flex-col bg-[#16181C] rounded-xl pt-2 mt-5">
+              <h2 className="p-2 text-lg font-bold">Who to Follow</h2>
+              {followProfiles.map((followProfile: any) => (
+                <Link key={followProfile.id} href={`${followProfile.username}`}>
+                  {/* lst:rounded-b-xl also applied to first element WHY ? :( */}
+                  <div className="flex space-x-3 p-2 w-full justify-center hover:bg-white/10 transition duration-200 last:rounded-b-xl">
+                    <div className="flex-none my-auto">
+                      <Image
+                        src={followProfile.avatar_url}
+                        width={40}
+                        height={40}
+                        alt="Profile Image"
+                        className="rounded-full"
+                      />
+                    </div>
+                    <div className="flex w-full justify-between">
+                      <div className="flex flex-col w-full">
+                        <h2 className="hover:underline transition duration-200">
+                          {followProfile.name}
+                        </h2>
+                        <h2 className="text-gray-500">
+                          @{followProfile.username}
+                        </h2>
+                      </div>
+                      <div className="flex flex-col w-fit justify-center items-end">
+                        <button className="rounded-full text-black font-medium bg-[#EFF3F4] px-4 py-1">
+                          Follow
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              ))}
             </div>
-            <div className="flex flex-col bg-[#16181C] rounded-xl mx-4 px-4 p-2 mt-5 space-y-4">
+            <div className="flex flex-col bg-[#16181C] rounded-xl px-4 p-2 mt-5 space-y-4">
               <h2>Trending</h2>
               <div>Trend 1</div>
               <div>Trend 2</div>
