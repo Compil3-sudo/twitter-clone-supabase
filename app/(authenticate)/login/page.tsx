7ca973194a85a7a5a9c1355b93e94bd4f577951c
tincurar <tincurares13@gmail.com>
2023-10-11 14:13:09 +0300

    styled sign-in / sign-up

--- app/(authenticate)/login/page.tsx
+++ app/(authenticate)/login/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import { BsGithub } from "react-icons/bs";
 // change to server component if you want to use the form
 // for email sign-up
 
 // import Link from "next/link";
 import Messages from "./messages";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useState } from "react";
+import { VscArrowLeft } from "react-icons/vsc";
 
 export default function Login() {
+  const [createAccount, setCreateAccount] = useState(false);
   const supabase = createClientComponentClient<Database>();
 
   async function signInWithGitHub() {
@@ -24,77 +28,152 @@ export default function Login() {
 
   return (
     <div className="flex flex-col w-fit self-center px-8 sm:max-w-md justify-center gap-2">
-      <h1>Change between login / create account</h1>
-      <h1>
-        Change this into a modal and ask for each detail after email is
-        confirmed
-      </h1>
-      <h2>make default profile picture</h2>
-      <h2>
-        generate random unique username based on name - let user change it later
-      </h2>
-      <form
-        className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-        action="/auth/sign-in"
-        method="post"
-      >
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-
-        <label className="text-md" htmlFor="email">
-          Name
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="name"
-          placeholder="name"
-          required
-        />
-
-        <h2>ask for this after email confirmed</h2>
-        <label className="text-md" htmlFor="email">
-          Username
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="username"
-          placeholder="username"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <button className="bg-green-700 rounded px-4 py-2 text-white mb-2">
-          Sign In
-        </button>
-        <button
-          formAction="/auth/sign-up"
-          className="border border-gray-700 rounded px-4 py-2 text-white mb-2"
-        >
-          Sign Up
-        </button>
-        <Messages />
-      </form>
+      <h1 className="text-5xl mb-6">Happening now</h1>
+      <h1 className="text-2xl mb-2">Join today.</h1>
       <button
         onClick={signInWithGitHub}
         className="bg-blue-500 rounded px-4 py-2 text-white mb-2"
       >
-        Login with Github
+        <div className="flex flex-row justify-center space-x-2">
+          <BsGithub size={20} className="my-auto" />
+          <span>Sign in with Github</span>
+        </div>
       </button>
+
+      <div className="flex items-center">
+        <div className="w-1/2 border-t border-gray-400"></div>
+        <div className="mx-4 text-gray-400">or</div>
+        <div className="w-1/2 border-t border-gray-400"></div>
+      </div>
+
+      {createAccount && (
+        <>
+          <div
+            className="flex flex-row py-2 space-x-2 cursor-pointer hover:bg-white/10 transition-all ease-out"
+            onClick={() => setCreateAccount(false)}
+          >
+            <VscArrowLeft size={20} className="my-auto" />
+            <span>Back to Sign in</span>
+          </div>
+          <h2 className="text-lg">Create your account</h2>
+        </>
+      )}
+
+      <form
+        className="flex-1 flex flex-col w-full justify-center gap-2"
+        action="/auth/sign-in"
+        method="post"
+      >
+        {/* <label
+          className="group h-16 focus-within:outline-blue-500 focus-within:outline-2 flex flex-col text-md outline outline-1 outline-slate-700 rounded-lg p-2 my-2"
+          htmlFor="email"
+        >
+          <span className="mt-3 text-xl group-focus-within:-translate-y-4 group-focus-within:text-base transition-all ease-out duration-600 group-focus-within:text-blue-500">
+            Email
+          </span>
+          <input
+            className="absolute mt-6 bg-inherit outline-none group"
+            type="email"
+            name="email"
+            required
+          />
+        </label> 
+        <label
+          className="group h-16 focus-within:outline-blue-500 focus-within:outline-2 flex flex-col text-md outline outline-1 outline-slate-700 rounded-lg p-2 my-2"
+          htmlFor="password"
+        >
+          <span className="absolute mt-3 text-xl group-focus-within:mt-0 group-focus-within:text-base transition duration-600 group-focus-within:text-blue-500">
+            Password
+          </span>
+          <input
+            className="absolute mt-6 bg-inherit outline-none group"
+            type="password"
+            name="password"
+            required
+          />
+        </label> */}
+        <label
+          className="group focus-within:outline-blue-500 focus-within:outline-2 flex flex-col text-md outline outline-1 outline-slate-700 rounded-lg p-2 my-2"
+          htmlFor="email"
+        >
+          <span className="group-focus-within:text-blue-500">Email</span>
+          <input
+            className="bg-inherit outline-none group"
+            type="email"
+            name="email"
+          />
+        </label>
+
+        <label
+          className="group focus-within:outline-blue-500 focus-within:outline-2 flex flex-col text-md outline outline-1 outline-slate-700 rounded-lg p-2 my-2"
+          htmlFor="password"
+        >
+          <span className="group-focus-within:text-blue-500">Password</span>
+          <input
+            className="bg-inherit outline-none group"
+            type="password"
+            name="password"
+          />
+        </label>
+
+        {!createAccount && (
+          <button className="bg-green-700 rounded px-4 py-2 text-white mb-2">
+            Sign In
+          </button>
+        )}
+
+        {!createAccount && (
+          <>
+            <h2 className="text-lg">Don't have an account?</h2>
+
+            <button
+              onClick={() => setCreateAccount(true)}
+              className="bg-blue-500 rounded px-4 py-2 text-white mb-2"
+            >
+              Create account
+            </button>
+          </>
+        )}
+
+        {createAccount && (
+          <>
+            <label
+              className="group focus-within:outline-blue-500 focus-within:outline-2 flex flex-col text-md outline outline-1 outline-slate-700 rounded-lg p-2 my-2"
+              htmlFor="username"
+            >
+              <span className="group-focus-within:text-blue-500">Username</span>
+              <input
+                className="bg-inherit outline-none group"
+                type="text"
+                name="username"
+                required={createAccount}
+              />
+            </label>
+            <label
+              className="group focus-within:outline-blue-500 focus-within:outline-2 flex flex-col text-md outline outline-1 outline-slate-700 rounded-lg p-2 my-2"
+              htmlFor="name"
+            >
+              <span className="group-focus-within:text-blue-500">Name</span>
+              <input
+                className="bg-inherit outline-none group"
+                type="text"
+                name="name"
+                required={createAccount}
+              />
+            </label>
+          </>
+        )}
+
+        {createAccount && (
+          <button
+            formAction="/auth/sign-up"
+            className="bg-blue-500 rounded px-4 py-2 text-white mb-2"
+          >
+            Sign Up
+          </button>
+        )}
+        <Messages />
+      </form>
     </div>
   );
 }
--- app/(authenticate)/page.tsx
+++ app/(authenticate)/page.tsx
@@ -1,7 +1,9 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { redirect } from "next/navigation";
 import Login from "./login/page";
+import Logo from "public/static/android-chrome-192x192.png";
 import { cookies } from "next/headers";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
@@ -18,7 +20,7 @@ export default async function LoginPage() {
   if (session) redirect("/home");
 
   return (
-    <div className="grid h-screen place-items-center">
+    <div className="grid grid-flow-col h-screen place-items-center">
       <div className="flex flex-col justify-center">
         <div className="self-center p-6">
           <h1 className="text-3xl font-bold py-2">Twitter Clone by Compil3</h1>
@@ -29,9 +31,12 @@ export default async function LoginPage() {
             <li>React</li>
             <li>TailwindCSS</li>
           </ul>
+          <div className="flex justify-center mt-5">
+            <Image src={Logo} width={192} height={192} alt="Logo" />
+          </div>
         </div>
-        <Login />
       </div>
+      <Login />
     </div>
   );
 }
