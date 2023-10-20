e3dff332369babfe5d7112b6f9086364ebce73a8
tincurar <tincurares13@gmail.com>
2023-10-20 19:46:06 +0300

    connect page shows profiles to follow from db

--- app/(twitter)/connect/page.tsx
+++ app/(twitter)/connect/page.tsx
@@ -1,29 +1,45 @@
 import ArrowHeader from "@/components/client-components/ArrowHeader";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import WhoToFollowProfile from "@/components/client-components/WhoToFollowProfile";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
-const Connect = () => {
+export const dynamic = "force-dynamic";
+
+const Connect = async () => {
   // TODO: OPTIONAL - nice feature add tabs for follow anyone? & follow for you with custom suggestions for auth user?
-  const supabase = createClientComponentClient();
+  const supabase = createServerComponentClient<Database>({ cookies });
 
-  const getCurrentUser = async () => {
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) return;
+  const userId = user.id;
 
-    return user;
-  };
+  const { data: followProfiles, error: followProfilesError } =
+    await supabase.rpc("get_profiles_to_follow", {
+      authenticated_user_id: userId, // Pass the authenticated user's ID
+      profile_limit: 100, // limit the number of profiles to fetch to 3
+    });
 
-  const test = supabase.auth.getUser().then((response) => response.data);
-  console.log(test);
-
-  const user = getCurrentUser();
-
-  console.log(user);
+  if (followProfilesError) console.log(followProfilesError);
 
   return (
     <>
       <ArrowHeader title="Connect" />
       <h1 className="text-xl p-4 font-bold">Who to follow</h1>
+      {followProfiles && followProfiles.length > 0 ? (
+        followProfiles.map((profile) => (
+          <WhoToFollowProfile
+            key={profile.id}
+            userId={userId}
+            followProfile={profile}
+            isUserFollowingProfile={false}
+            showBio={true}
+          />
+        ))
+      ) : (
+        <h1>No Profiles to follow.</h1>
+      )}
     </>
   );
 };
