ed7c5e8c446134e8195be89271714b8d513c9179
Compil3-sudo <tincurares13@gmail.com>
2025-09-22 23:08:03 +0200

    fixed z index issue. emoji panel was overlapped by tweet images

--- components/client-components/CustomTextArea.tsx
+++ components/client-components/CustomTextArea.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
-import { BsEmojiSmile } from "react-icons/bs";
-import { GoFileMedia } from "react-icons/go";
-import { VscClose } from "react-icons/vsc";
-import Picker from "@emoji-mart/react";
-import emojiData from "@emoji-mart/data";
-import { init } from "emoji-mart";
+import { useEffect, useState } from 'react';
+import { BsEmojiSmile } from 'react-icons/bs';
+import { GoFileMedia } from 'react-icons/go';
+import { VscClose } from 'react-icons/vsc';
+import Picker from '@emoji-mart/react';
+import emojiData from '@emoji-mart/data';
+import { init } from 'emoji-mart';
 
 init({ emojiData });
 
@@ -43,7 +43,7 @@ const CustomTextArea = ({
 
   const buttonStatus =
     submitting ||
-    (txtAreaTextRef.current?.value.trim() === "" && !media) ||
+    (txtAreaTextRef.current?.value.trim() === '' && !media) ||
     remainingChars < 0;
 
   const countCharacters = () => {
@@ -51,18 +51,18 @@ const CustomTextArea = ({
   };
 
   useEffect(() => {
-    txtAreaTextRef.current!.addEventListener("input", countCharacters);
+    txtAreaTextRef.current!.addEventListener('input', countCharacters);
 
     return () => {
       if (txtAreaTextRef.current) {
-        txtAreaTextRef.current.removeEventListener("input", countCharacters);
+        txtAreaTextRef.current.removeEventListener('input', countCharacters);
       }
     };
   }, [txtAreaTextRef.current?.value.length]);
 
   const handleChange = () => {
     if (txtAreaTextRef.current) {
-      txtAreaTextRef.current.style.height = "auto";
+      txtAreaTextRef.current.style.height = 'auto';
       txtAreaTextRef.current.style.height = `${Math.min(
         txtAreaTextRef.current.scrollHeight,
         maxTextAreaHeight
@@ -105,7 +105,7 @@ const CustomTextArea = ({
     const formData = new FormData();
     formData.append(txtAreaName, txtAreaTextRef.current!.value);
     if (media) {
-      formData.append("media", media);
+      formData.append('media', media);
     }
 
     setRemainingChars(txtAreaMaxLength); // reset remainingChars
@@ -117,58 +117,61 @@ const CustomTextArea = ({
     <form
       // action={formAction}
       onSubmit={handleSubmit}
-      className="flex flex-col w-full px-2 pt-2"
+      className='flex flex-col w-full px-2 pt-2'
     >
-      <div className="flex flex-col w-full">
-        <div className="flex flex-col border-b">
+      <div className='flex flex-col w-full'>
+        <div className='flex flex-col border-b'>
           <textarea
             ref={txtAreaTextRef}
             onChange={handleChange}
             maxLength={characterLimit}
             name={txtAreaName}
-            className="bg-transparent no-scrollbar border-none outline-none resize-none pt-2"
+            className='bg-transparent no-scrollbar border-none outline-none resize-none pt-2'
             placeholder={txtAreaPlaceholder}
           />
           {media && (
-            <div className="my-4 relative">
+            <div className='my-4 relative'>
               <div
                 onClick={() => {
                   onRemoveMedia();
                   if (!txtAreaTextRef.current?.value.length || submitting)
                     setIsButtonDisabled(true);
                 }}
-                className="absolute right-2 top-2 w-fit p-2 bg-slate-700 hover:bg-opacity-90 rounded-full cursor-pointer"
+                className='absolute right-2 top-2 w-fit p-2 bg-slate-700 hover:bg-opacity-90 rounded-full cursor-pointer'
               >
                 <VscClose size={20} />
               </div>
-              {media.type.startsWith("image/") ? (
+              {media.type.startsWith('image/') ? (
                 <img
                   src={URL.createObjectURL(media)}
-                  alt="Media Preview"
-                  className="w-full h-auto"
+                  alt='Media Preview'
+                  className='w-full h-auto'
                 />
-              ) : media.type.startsWith("video/") ? (
+              ) : media.type.startsWith('video/') ? (
                 <video controls>
-                  <source src={URL.createObjectURL(media)} type={media.type} />
+                  <source
+                    src={URL.createObjectURL(media)}
+                    type={media.type}
+                  />
                 </video>
               ) : null}
             </div>
           )}
         </div>
 
-        <div className="flex flex-row justify-between mt-4">
-          <div className="flex">
+        <div className='flex flex-row justify-between mt-4'>
+          <div className='flex'>
             <label
-              htmlFor="addMedia"
-              className="my-auto p-2 hover:bg-blue-500/20 rounded-full transition ease-out text-blue-500 cursor-pointer"
+              htmlFor='addMedia'
+              className='my-auto p-2 hover:bg-blue-500/20 rounded-full transition ease-out text-blue-500 cursor-pointer'
             >
               <GoFileMedia size={20} />
               <input
-                type="file"
-                id="addMedia"
-                name="media"
-                className="hidden absolute"
-                accept="image/* video/mp4"
+                type='file'
+                id='addMedia'
+                name='media'
+                className='hidden absolute'
+                accept='image/* video/mp4'
                 onChange={uploadMedia}
                 disabled={uploading}
               />
@@ -176,38 +179,38 @@ const CustomTextArea = ({
 
             <div
               onClick={() => setShowEmojiMenu(true)}
-              className="relative my-auto p-2 hover:bg-blue-500/20 rounded-full transition ease-out text-blue-500 cursor-pointer"
+              className='relative my-auto p-2 hover:bg-blue-500/20 rounded-full transition ease-out text-blue-500 cursor-pointer'
             >
               <BsEmojiSmile size={20} />
               {showEmojiMenu && (
-                <div className="absolute top-12 left-0">
+                <div className='absolute top-12 left-0 z-10'>
                   <Picker
                     data={emojiData}
                     onClickOutside={() => setShowEmojiMenu(false)}
                     onEmojiSelect={(emoji: EmojiType) => addEmoji(emoji)}
-                    previewEmoji={"wave"}
+                    previewEmoji={'wave'}
                   />
                 </div>
               )}
             </div>
           </div>
 
-          <div className="flex">
+          <div className='flex'>
             {remainingChars <= 20 && remainingChars > 0 ? (
-              <p className="py-2 px-4 text-yellow-500">{remainingChars}</p>
+              <p className='py-2 px-4 text-yellow-500'>{remainingChars}</p>
             ) : (
               remainingChars <= 0 && (
-                <p className="py-2 px-4 text-red-500">{remainingChars}</p>
+                <p className='py-2 px-4 text-red-500'>{remainingChars}</p>
               )
             )}
 
             <button
               disabled={isButtonDisabled}
-              type="submit"
+              type='submit'
               className={`${
                 isButtonDisabled
-                  ? "bg-opacity-50 text-foreground/50"
-                  : "hover:bg-opacity-80 transition ease-in-out"
+                  ? 'bg-opacity-50 text-foreground/50'
+                  : 'hover:bg-opacity-80 transition ease-in-out'
               } rounded-full bg-blue-500 py-2 px-4`}
             >
               {buttonText}
